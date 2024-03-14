df250e6289c15eb90b73c8c17136b9e498ee061c
swe2mohamed <swe.mohamed1990@gmail.com>
2024-03-14 16:42:30 +0100

    8th Version update Method is Finish

--- src/main/java/se/Java_Array_workshop/NameRepository.java
+++ src/main/java/se/Java_Array_workshop/NameRepository.java
@@ -1,6 +1,7 @@
 package se.Java_Array_workshop;
 import java.util.Arrays;
 import java.util.Objects;
+import java.util.Scanner;
 
 public class NameRepository {
     private static String[] names = new String[0];
@@ -78,9 +79,9 @@ public class NameRepository {
         String[] fullNameArray = NameRepository.names;
         for (String name : fullNameArray) {
             String[] fullName = name.split(" ");
-                if (Objects.equals(fullName[0], firstName)) {
-                    count += 1;
-                }
+            if (Objects.equals(fullName[0], firstName)) {
+                count += 1;
+            }
         }
         String[] findLastNameArray = new String[count];
         int index = 0;
@@ -94,7 +95,7 @@ public class NameRepository {
         return findLastNameArray;
     }
 
-    public static String[] findByLastName(final String lastName){
+    public static String[] findByLastName(final String lastName) {
         // todo: Searches the array trying to find all names that has passed in last name. Returns a String array containing all.
         int count = 0;
         String[] fullNameArray = NameRepository.names;
@@ -115,4 +116,27 @@ public class NameRepository {
         }
         return findLastNameArray;
     }
+
+    public static boolean update(final String original, final String updatedName) {
+        // todo: Should find a name and replace it with new fullName if available. Return true if name was found and updated with the new name. False if name could not be updated because name wasn't found or name was found but an existing name matching the updatedName already exists.
+        String[] names = NameRepository.names;
+        for (String name : names){
+            if (name.equals(updatedName)){
+                return false;
+            }
+        }
+
+        if (!updatedName.equals(original)){
+            for (int i = 0; i < names.length; i++) {
+                if (original.equals(names[i])) {
+                        names[i] = updatedName;
+                        return true;
+                }
+            }
+        }
+        return false;
+    }
 }
+
+
+
